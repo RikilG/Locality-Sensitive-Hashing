078772925b577c853f5a7c1d49b8b6b81f1ef2ea
Rikil G <rikil.gajarla@gmail.com>
2019-10-13 16:50:34 +0530

    document code for shingling
    documentation is written for shingling functions.
    also added ullman ch3 reference for lsh implementation

--- shingling.py
+++ shingling.py
@@ -1,39 +1,57 @@
 """
-This module takes the path of corpus, reads the documents and
-builds a index for presence of shingles
+Incidence matrix builder for corpus using shingling
+
+This module contains the following functions:
+    * list_files - list the files in the given directory
+    * get_shingle_matrix - returns incidence-matrix of shingle and documents
 """
 
 from pandas import DataFrame,Series
 import codecs
 import os
 
+
 def list_files(folderpath):
     """
-    This function reads and builds corpus from given folderpath
+    Reads and builds corpus files list from given folderpath
+
+    Parameters
+    ----------
+    folderpath: str
+        The path to target folder where corpus files exist
+    
+    Returns
+    -------
+    list
+        a list of files present in the directory(includes sub-folders)
     """
+
     print("Reading corpus")
     # check if folder path exists
     if(not os.path.exists(folderpath)):
-        print("given folder path does not exist")
+        raise Exception(f"Given folder path: {folderpath} does not exist")
         return
     
     doc_files = []
-    i = 0
+    i = 0           # index/id for each file
     
     for (root, dirs, files) in os.walk(folderpath):
         for f in files:
-            doc_files.append((os.path.join(root, f), i))
-            i += 1
+            if f.endswith(".txt"):
+                doc_files.append( (os.path.join(root, f), i) )
+                i += 1
     
     return doc_files
 
+
 def build_matrix(files, k=4, newline=False):
     """
-    build incidence matrix for k-grams (shingles)
+    helper-function: build incidence matrix for k-grams (shingles)
     """
+
     df = DataFrame()
     for f in files:
-        with codecs.open( f[0],'r', encoding="utf8",errors='ignore' ) as doc:
+        with codecs.open(f[0], 'r', encoding="utf8", errors='ignore') as doc:
             print("reading: "+f[0])
             str = doc.read()
             df[ f[1] ] = 0
@@ -49,10 +67,34 @@ def build_matrix(files, k=4, newline=False):
     
     return df
 
+
+def get_shingle_matrix(folderpath, shingle_size=8):
+    """
+    Performs shingling and builds incidence index for shingles
+
+    Parameters
+    ----------
+    folderpath: str
+        The path to target folder where corpus files exist
+    shingle_size: int
+        Size of shingles to divide the documents into
+    
+    Returns
+    -------
+    Dataframe
+        pandas dataframe containing rows as shingles and cols as doc_ids
+    """
+
+    files = list_files(folderpath)
+    incidence_matrix = build_matrix(files, k=shingle_size)
+    return incidence_matrix
+
+
 def main():
-    files = list_files("corpus")
-    incidence_matrix = build_matrix(files, k=8)
+    folderpath = input("Enter folderpath for corpus: ")
+    incidence_matrix = get_shingle_matrix(folderpath)
     print(incidence_matrix)
+
 
 if __name__ == "__main__":
     main()
