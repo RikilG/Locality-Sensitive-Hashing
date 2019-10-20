07172f0b13937e29e8f6cf6532a89f583572a40b
rohith203 <saranga.rohith@gmail.com>
2019-10-20 22:48:47 +0530

    add design doc and removed parallelization

--- shingling.py
+++ shingling.py
@@ -7,7 +7,6 @@ This module contains the following functions:
 """
 
 from pandas import DataFrame,read_pickle
-import multiprocessing as mp
 import numpy as np
 import codecs
 import os
@@ -40,7 +39,7 @@ def list_files(folderpath, extension=".txt"):
     print(f"Reading corpus: {folderpath}")
     # check if folder path exists
     if(not os.path.exists(folderpath)):
-        raise Exception(f"Given folder path: '{folderpath}' does not exist")
+        raise Exception(f"Given folder path: {folderpath} does not exist")
         return
     
     doc_files = []
@@ -57,75 +56,11 @@ def list_files(folderpath, extension=".txt"):
     
     return doc_files
 
-
-# for parallel file IO. useful if using a RAID system or disk supports parallel
-# reading of files
-def parallel_file_read(t_id, file_list, k, output, newline=False):
-    """helper-function for parallelism
-    """
-
-    for file_tuple in file_list:
-        with codecs.open(file_tuple[0], 'r', encoding="utf8", errors='ignore') as doc:
-            # print(f"reading: {file_tuple[0]}")
-            data = doc.read()
-            data = data.lower()
-            data = ' '.join(data.split())   # substiture multiples spaces with single space
-            data = data.replace('\r\n', ' ') # replace windows line endings with space
-            data = data.replace('\r', '')   # remove \r in windows
-            data = data.replace('\t', '')   # remove tab-spaces
-            if newline is False:
-                data.replace('\n', ' ')
-            for i in range(0, len(data)-k+1):
-                shingle = data[i:i+k]
-                output.put((shingle, file_tuple[1]))
-        # print(f"Done: {file_tuple}")
-    
-    output.put(('-1', t_id))
-
-
-# for parallel file IO. useful if using a RAID system or disk supports parallel
-# reading of files
-def parallel_adapter(files, k, df, newline, pool=3):
-    """helper-function for parallelism
-    """
-    output = mp.Queue()
-    process_files = np.array_split(np.array(files), pool)
-    processes = [ mp.Process(target=parallel_file_read, args=(i, process_files[i], k, output, newline)) for i in range(pool) ]
-
-    for p in processes:
-        p.start()
-    
-    x = pool
-    while x > 0:
-        temp = output.get()
-        if df.shape[0]%2000 == 0:
-            print(df.shape)
-        if temp[0] == '-1':
-            x -= 1
-            processes[ temp[1] ].join()
-            print("thread done")
-            continue
-        if temp[1] not in df.columns:
-            df[ temp[1] ] = 0
-        if temp[0] not in df.index:
-            df.loc[ temp[0] ] = [ 0 for i in range(df.shape[1]) ]
-        df.loc[ temp[0], temp[1] ] = 1
-    
-    print("Done with all process")
-    for p in processes:
-        p.join()
-    
-    return df
-
-
-def build_matrix(files, k=4, newline=False, parallel=True, pool=3):
+def build_matrix(files, k=4, newline=False):
     """helper-function: build incidence matrix for k-grams (shingles)
     """
 
     df = DataFrame(columns=[x[1] for x in files])
-
-    if parallel is True:
-        return parallel_adapter(files, k, df, newline, pool)
 
     for f in tqdm(files):
         with codecs.open(f[0], 'r', encoding="utf8", errors='ignore') as doc:
@@ -151,7 +86,7 @@ def build_matrix(files, k=4, newline=False, parallel=True, pool=3):
     return df
 
 
-def get_shingle_matrix(folderpath, shingle_size=8, extension=".txt", parallel=0):
+def get_shingle_matrix(folderpath, shingle_size=8, extension=".txt"):
     """Performs shingling and builds incidence index for shingles
 
     if a already generated pickle file named {foldername}_inc_mat.pickle exists,
@@ -191,10 +126,7 @@ def get_shingle_matrix(folderpath, shingle_size=8, extension=".txt", parallel=0)
     # fetch the list of files to be read
     files = list_files(folderpath, extension)
     # check if parallelism is requested
-    if parallel == 0:
-        incidence_matrix = build_matrix(files, k=shingle_size, parallel=False)
-    else:
-        incidence_matrix = build_matrix(files, k=shingle_size, parallel=True, pool=parallel)
+    incidence_matrix = build_matrix(files, k=shingle_size)
 
     print("saving generated incidence index to file...")
     incidence_matrix.to_pickle(f"{folderpath}_inc_mat.pickle")
@@ -212,7 +144,6 @@ def main():
     incidence_matrix = get_shingle_matrix(folderpath)
     print(f"endtime: {time() - st_time}")
     print(incidence_matrix)
-
 
 
 if __name__ == "__main__":
